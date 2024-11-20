d72df2cffb09a1d68ec0b8dc931406625cad46bb
Dev22Patel <notrealdev2211@gmail.com>
2024-11-20 19:13:02 +0530

    minor changes

--- DevWeb/src/pages/Hero.tsx
+++ DevWeb/src/pages/Hero.tsx
@@ -6,13 +6,13 @@ export function Hero() {
       <div className="space-y-1">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
           Trust me, I'm a{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-300 to-purple-400 inline-block">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-green-600 to-green-800  dark:bg-gradient-to-r dark:from-cyan-300 dark:to-purple-400 inline-block">
             web-developer.
           </span>
         </h1>
       </div>
       <div className="space-y-6">
-        <p className="text-base sm:text-lg text-zinc-400 max-w-2xl leading-relaxed">
+        <p className="text-base sm:text-lg text-zinc-800 dark:text-zinc-400 max-w-2xl leading-relaxed">
           Meet Dev Patel, a passionate web developer and Computer Engineering student at DDIT.
           As a 5th-semester student and a member of the web development team at the{" "}
             Google Developer Student Club (GDSC)
